2cc3b8da954d86fa0264f41d594ed8f0e70ee35d
kaellandrade <micael.santos@dcomp.ufs.br>
2021-05-10 17:59:28 -0300

    configurando babel

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -6,12 +6,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from '../main/Routes';
 
 const App = _ => (
-    <div className="wrapper">
         <Router>
             <Routes />
         </Router>
-
-    </div>
 );
 
 export default App;
