fc8d492f5c47a8916708f0496a16a0bd90eb4364
kaellandrade <micael.santos@dcomp.ufs.br>
2021-05-18 20:33:17 -0300

    ajustanto as rotas

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/estilo';
-
+import '@fortawesome/fontawesome-free/css/all.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from '../main/Routes';
 import Header from '../common/template/Header';
--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -6,14 +6,13 @@ import Grid from '../common/layout/Grid';
 
 import { Switch, Route } from "react-router-dom";
 import Content from '../common/template/Content';
+import Login from '../login/Login';
 
 const Routes = props => (
     <Switch>
         <Route exact path="/">
             <Content className='tela-login'>
-                <Container>
-                    <h2>Login Tela Login</h2>
-                </Container>
+                <Login/> {/* Tela de login*/}
             </Content>
         </Route>
 
